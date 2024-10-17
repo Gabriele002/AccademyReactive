1b9dda0b39ffbe1aca5936b485c704c2c096a47b
ITMASTER\G.Ciarleglio-cons <g.ciarleglio-cons@reactive-almaviva.it>
2024-10-17 11:48:52 +0200

    Prova per vedere se il flag e attivo

--- EserciziAcademy/academy/esercizi/Esercizio_1_1.java
+++ EserciziAcademy/academy/esercizi/Esercizio_1_1.java
@@ -1,6 +1,6 @@
 package academy.esercizi;
 public class Esercizio_1_1 {
     public static void main(String[] args) {
-        System.out.println("Sono Gabriele!");
+        System.out.println("Sono Gabrielee!");
     }
 }
