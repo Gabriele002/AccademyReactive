6c5b9b5ca7c6c75ac025c42fc4b2b6cdbf8eef6a
ITMASTER\G.Ciarleglio-cons <g.ciarleglio-cons@reactive-almaviva.it>
2024-10-21 20:11:07 +0200

    Completato esercizio 9_1 creazione di un metodo calcola tasse sia per la variante italiana sia per la variante americano tenendo conto della tassazione diversa se conioge o non

---
+++ EserciziAcademy/academy/esercizi/Esercizio_6_1.java
@@ -0,0 +1,16 @@
+package academy.esercizi;
+
+public class Esercizio_6_1 {
+    public static void main(String[] args) {
+                System.out.println("------------------------");
+                System.out.printf("%-4s %-4s %-4s %-4s%n", "x1", "x2", "x3", "x4");
+                System.out.println("------------------------");
+                System.out.printf("%-4s %-4s %-4s %-4s %n", "1", "1", "1", "1");
+                System.out.printf("%-4s %-4s %-4s %-4s %n", "2", "4", "8", "16");
+                System.out.printf("%-4s %-4s %-4s %-4s %n", "3", "9", "27", "81");
+                System.out.printf("%-4s %-4s %-4s %-4s %n", "...", "...", "...", "...");
+                System.out.printf("%-4s %-4s %-4s %-4s %n", "10", "100", "1000", "10000");
+                System.out.println("------------------------");
+    }
+}
+
---
+++ EserciziAcademy/academy/esercizi/Esercizio_9_1.java
@@ -0,0 +1,132 @@
+package academy.esercizi;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Esercizio_9_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nazionalita;
+        BigDecimal ral;
+
+        while (true) {
+            System.out.print("Inserisci la nazionalità (Italiana o Americana): ");
+            nazionalita = scanner.nextLine();
+
+            if (nazionalita.equalsIgnoreCase("Italiana") || nazionalita.equalsIgnoreCase("Americana")) {
+                System.out.print("Inserisci il tuo RAL (deve essere positivo): ");
+                ral = scanner.nextBigDecimal();
+
+                if (ral.compareTo(BigDecimal.ZERO) > 0) {
+                    break;
+                } else {
+                    System.out.println("Il RAL deve essere un valore positivo. Riprova.");
+                }
+            } else {
+                System.out.println("Nazionalità non valida. Riprova.");
+                System.out.println();
+            }
+        }
+
+
+        BigDecimal tasse = calcolaTasse(nazionalita, ral);
+        if (tasse != null) {
+            BigDecimal netto = ral.subtract(tasse);
+            stampaTabella(ral, tasse, netto);
+        }
+
+    }
+
+    public static BigDecimal calcolaTasse(String nazionalita, BigDecimal ral) {
+        BigDecimal tasse = BigDecimal.ZERO;
+            //Nazionalita italiana
+        if (nazionalita.equalsIgnoreCase("Italiana")) {
+            BigDecimal soglia15000 = BigDecimal.valueOf(15000);
+            BigDecimal soglia28000 = BigDecimal.valueOf(28000);
+            BigDecimal soglia50000 = BigDecimal.valueOf(50000);
+
+            BigDecimal aliquota23 = BigDecimal.valueOf(0.23);
+            BigDecimal aliquota25 = BigDecimal.valueOf(0.25);
+            BigDecimal aliquota35 = BigDecimal.valueOf(0.35);
+            BigDecimal aliquota43 = BigDecimal.valueOf(0.43);
+
+            if (ral.compareTo(soglia15000) <= 0) {
+                tasse = ral.multiply(aliquota23);
+            } else if (ral.compareTo(soglia28000) <= 0) {
+                BigDecimal tasseFino15000 = soglia15000.multiply(aliquota23);
+                BigDecimal tasseSopra15000 = ral.subtract(soglia15000).multiply(aliquota25);
+                tasse = tasseFino15000.add(tasseSopra15000);
+            } else if (ral.compareTo(soglia50000) <= 0) {
+                BigDecimal tasseFino15000 = soglia15000.multiply(aliquota23);
+                BigDecimal tasseFino28000 = soglia28000.subtract(soglia15000).multiply(aliquota25);
+                BigDecimal tasseSopra28000 = ral.subtract(soglia28000).multiply(aliquota35);
+                tasse = tasseFino15000.add(tasseFino28000).add(tasseSopra28000);
+            } else {
+                BigDecimal tasseFino15000 = soglia15000.multiply(aliquota23);
+                BigDecimal tasseFino28000 = soglia28000.subtract(soglia15000).multiply(aliquota25);
+                BigDecimal tasseFino50000 = soglia50000.subtract(soglia28000).multiply(aliquota35);
+                BigDecimal tasseSopra50000 = ral.subtract(soglia50000).multiply(aliquota43);
+                tasse = tasseFino15000.add(tasseFino28000).add(tasseFino50000).add(tasseSopra50000);
+            }
+            // Nazionalita Americana
+        }else if (nazionalita.equalsIgnoreCase("Americana")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Sei coniugato? (sì/no): ");
+            String risposta = scanner.next().toLowerCase();
+            boolean statoCivile = risposta.equals("sì");
+            if (statoCivile) {
+                // Tassazione per coniugati
+                BigDecimal soglia16000 = BigDecimal.valueOf(16000);
+                BigDecimal soglia64000 = BigDecimal.valueOf(64000);
+
+                BigDecimal aliquota10 = BigDecimal.valueOf(0.10);
+                BigDecimal aliquota15 = BigDecimal.valueOf(0.15);
+                BigDecimal aliquota25 = BigDecimal.valueOf(0.25);
+
+                if (ral.compareTo(soglia16000) <= 0) {
+                    tasse = ral.multiply(aliquota10);
+                } else if (ral.compareTo(soglia64000) <= 0) {
+                    BigDecimal tasseFino16000 = soglia16000.multiply(aliquota10);
+                    BigDecimal tasseSopra16000 = ral.subtract(soglia16000).multiply(aliquota15);
+                    tasse = tasseFino16000.add(tasseSopra16000);
+                } else {
+                    BigDecimal tasseFino16000 = soglia16000.multiply(aliquota10);
+                    BigDecimal tasseFino64000 = soglia64000.subtract(soglia16000).multiply(aliquota15);
+                    BigDecimal tasseSopra64000 = ral.subtract(soglia64000).multiply(aliquota25);
+                    tasse = tasseFino16000.add(tasseFino64000).add(tasseSopra64000);
+                }
+            } else {
+                // Tassazione per non coniugati
+                BigDecimal soglia8000 = BigDecimal.valueOf(8000);
+                BigDecimal soglia32000 = BigDecimal.valueOf(32000);
+
+                BigDecimal aliquota10 = BigDecimal.valueOf(0.10);
+                BigDecimal aliquota15 = BigDecimal.valueOf(0.15);
+                BigDecimal aliquota25 = BigDecimal.valueOf(0.25);
+
+                if (ral.compareTo(soglia8000) <= 0) {
+                    tasse = ral.multiply(aliquota10);
+                } else if (ral.compareTo(soglia32000) <= 0) {
+                    BigDecimal tasseFino8000 = soglia8000.multiply(aliquota10);
+                    BigDecimal tasseSopra8000 = ral.subtract(soglia8000).multiply(aliquota15);
+                    tasse = tasseFino8000.add(tasseSopra8000);
+                } else {
+                    BigDecimal tasseFino8000 = soglia8000.multiply(aliquota10);
+                    BigDecimal tasseFino32000 = soglia32000.subtract(soglia8000).multiply(aliquota15);
+                    BigDecimal tasseSopra32000 = ral.subtract(soglia32000).multiply(aliquota25);
+                    tasse = tasseFino8000.add(tasseFino32000).add(tasseSopra32000);
+                }
+            }
+        }
+
+        return tasse;
+    }
+
+    private static void stampaTabella(BigDecimal ral, BigDecimal tasse, BigDecimal netto) {
+        System.out.println("-----------------------------------------");
+        System.out.printf("%-10s %-10s %-10s%n", "RAL", "Tasse", "Netto");
+        System.out.println("-----------------------------------------");
+        System.out.printf("%-10s %-10s %-10s%n", ral, tasse, netto);
+        System.out.println("-----------------------------------------");
+    }
+}
