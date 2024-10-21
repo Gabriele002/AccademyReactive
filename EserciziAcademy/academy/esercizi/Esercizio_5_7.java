d1ec84ee1527c699add3140980180b3e33620428
ITMASTER\G.Ciarleglio-cons <g.ciarleglio-cons@reactive-almaviva.it>
2024-10-21 17:02:52 +0200

    Implementato metodo che mi permette di creare una nuova stringa rimuovendone i trattini

---
+++ EserciziAcademy/academy/esercizi/Esercizio_5_7.java
@@ -0,0 +1,22 @@
+package academy.esercizi;
+
+public class Esercizio_5_7 {
+        public static void main(String[] args) {
+            String creditCardNumber ="4123-5678-9012-3450";
+            String senzaTrattini = rimuoviTrattini(creditCardNumber);
+            System.out.println("Stringa originale: " + creditCardNumber);
+            System.out.println("Stringa senza trattini: " + senzaTrattini);
+        }
+
+        public static String rimuoviTrattini(String str) {
+            StringBuilder stringaFormattata = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                char carattere = str.charAt(i);
+                if (carattere != '-') {
+                    stringaFormattata.append(carattere);
+                }
+            }
+            return stringaFormattata.toString();
+        }
+}
+
