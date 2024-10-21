75284167d18db868ea47c451b822c7a50ceeadd7
ITMASTER\G.Ciarleglio-cons <g.ciarleglio-cons@reactive-almaviva.it>
2024-10-21 16:24:25 +0200

    Implementato metodo che restituisce i numeri vicino alle coordinate date

--- EserciziAcademy/academy/esercizi/Esercizio_3_1.java
+++ EserciziAcademy/academy/esercizi/Esercizio_3_1.java
@@ -1,12 +1,19 @@
 package academy.esercizi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Esercizio_3_1 {
 
 
     public static void main(String[] args) {
-        int[][] tabellone = new int[10][9];
+        int[][] tabellone = new int[9][10];
         popolaTabellone(tabellone);
         stampaTabellone(tabellone);
+
+        List<Integer> vicini = trovaNumeriVicini(tabellone, 8, 8);
+        System.out.println("Numeri vicini: " + vicini);
+
     }
 
     public static void popolaTabellone(int[][] tabellone) {
@@ -27,12 +34,34 @@ public class Esercizio_3_1 {
         for (int i = 0; i < tabellone.length; i++) {
             for (int j = 0; j < tabellone[i].length; j++) {
                 if (tabellone[i][j] != 0) {
-                    System.out.printf("%4d ", tabellone[i][j]);
+                    System.out.printf("%2d ", tabellone[i][j]);
                 }
             }
             System.out.println();
         }
     }
 
+    public static List<Integer> trovaNumeriVicini(int[][] tabellone, int x, int y) {
+        List<Integer> vicini = new ArrayList<>();
 
+        if (x - 1 >= 0){
+            vicini.add(tabellone[x - 1][y]);
+        }
+
+        if (x + 1 < tabellone.length){
+            vicini.add(tabellone[x + 1][y]);
+        }
+        if (y - 1 >= 0){
+            vicini.add(tabellone[x][y - 1]);
+        }
+        if (y + 1 < tabellone[0].length){
+            vicini.add(tabellone[x][y + 1]);
+        }
+
+        if (vicini.isEmpty()) {
+            vicini.add(-1);
+        }
+
+        return vicini;
+    }
 }
