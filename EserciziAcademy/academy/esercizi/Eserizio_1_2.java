5ea5ca6c9ac5bba59c38b9d1bd9b58296545e476
ITMASTER\G.Ciarleglio-cons <g.ciarleglio-cons@reactive-almaviva.it>
2024-10-21 14:26:49 +0200

    Reso il programma piu versatile, permettendo all utente di inserire i dati

--- EserciziAcademy/academy/esercizi/Eserizio_1_2.java
+++ EserciziAcademy/academy/esercizi/Eserizio_1_2.java
@@ -2,6 +2,7 @@ package academy.esercizi;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Scanner;
 
 public class Eserizio_1_2 {
 
@@ -9,18 +10,29 @@ public class Eserizio_1_2 {
         calcolaInteressi();
     }
 
-    private static void calcolaInteressi() {
-        BigDecimal saldoIniziale = new BigDecimal("1000.00");
-        BigDecimal tassoInteresse = new BigDecimal("0.05");
+    public static void calcolaInteressi() {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Inserisci il numero di anni: ");
+        int anni = scn.nextInt();
+
+        System.out.print("Inserisci il tasso di interesse: ");
+        BigDecimal tassoInteressePercentuale = scn.nextBigDecimal();
+        BigDecimal tassoInteresse = tassoInteressePercentuale.divide(BigDecimal.valueOf(100));
+
+
+        System.out.print("Inserisci il saldo iniziale: ");
+        BigDecimal saldoIniziale = scn.nextBigDecimal();
 
         BigDecimal saldoFinale = saldoIniziale;
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= anni; i++) {
             BigDecimal interessiAnno = saldoFinale.multiply(tassoInteresse).setScale(2, RoundingMode.HALF_UP);
             saldoFinale = saldoFinale.add(interessiAnno);
             System.out.printf("Anno %d: Interessi guadagnati: %.2f euro, Saldo finale: %.2f euro %n",
                     i, interessiAnno, saldoFinale);
         }
+        scn.close();
     }
 
 }
