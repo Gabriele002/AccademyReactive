c4ee660382d0dfd3886d85ce5c32b16ea11d9a05
ITMASTER\G.Ciarleglio-cons <g.ciarleglio-cons@reactive-almaviva.it>
2024-10-22 14:29:17 +0200

    Esercizio 3_1 corretto

--- EserciziAcademy/academy/esercizi/esercizi_3/Esercizio_3_1.java
+++ EserciziAcademy/academy/esercizi/esercizi_3/Esercizio_3_1.java
@@ -11,8 +11,8 @@ public class Esercizio_3_1 {
         popolaTabellone(tabellone);
         stampaTabellone(tabellone);
 
-        List<Integer> vicini = trovaNumeriVicini(tabellone, 8, 8);
-        System.out.println("Numeri vicini: " + vicini);
+        trovaNumeriVicini(tabellone, 8, 8);
+        trovaCoordinate(tabellone, 5);
 
     }
 
@@ -41,27 +41,44 @@ public class Esercizio_3_1 {
         }
     }
 
-    public static List<Integer> trovaNumeriVicini(int[][] tabellone, int x, int y) {
-        List<Integer> vicini = new ArrayList<>();
-
+    public static void trovaNumeriVicini(int[][] tabellone, int x, int y) {
+        System.out.println("Numeri vicini: ");
         if (x - 1 >= 0){
-            vicini.add(tabellone[x - 1][y]);
+            System.out.println( tabellone[x-1][y]);
+        } else{
+            System.out.println("x");
         }
 
         if (x + 1 < tabellone.length){
-            vicini.add(tabellone[x + 1][y]);
+            System.out.println(tabellone[x + 1][y]);
+        } else{
+            System.out.println("x");
         }
         if (y - 1 >= 0){
-            vicini.add(tabellone[x][y - 1]);
+            System.out.println(tabellone[x][y - 1]);
+        } else{
+            System.out.println("x");
         }
         if (y + 1 < tabellone[0].length){
-            vicini.add(tabellone[x][y + 1]);
+            System.out.println(tabellone[x][y + 1]);
+        } else{
+            System.out.println("x");
         }
-
-        if (vicini.isEmpty()) {
-            vicini.add(-1);
-        }
-
-        return vicini;
     }
-}
+
+    public static void trovaCoordinate(int[][] tabellone,int numero) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < tabellone.length; i++) {
+            for (int j = 0; j < tabellone[i].length;j++) {
+                if (tabellone[i][j] == numero ){
+                    x = i;
+                    y = j;
+                }
+            }
+
+        }
+        trovaNumeriVicini(tabellone,x,y);
+    }
+
+    }
